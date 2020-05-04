29608e2372e90ed7808a6ec559c3e21ca69d86c4
delightech <hisafumi.kikkawa@gmail.com>
2020-05-04 13:16:36 +0900

    MODIFY if satement

--- sansu.rb
+++ sansu.rb
@@ -1,4 +1,13 @@
 # 二桁の繰り上がりの計算（小１〜小２用）の練習問題作成スクリプト
+require 'optparse'
+
+# commnad line option
+params = {}
+opt = OptionParser.new
+opt.on('-q', 'questions(optional)') { |v| params[:questions] = v }
+opt.on('-a', 'answers(optional)') { |v| params[:answers] = v }
+opt.parse!(ARGV)
+
 arr = []
 idx = 0
 while true
@@ -8,11 +17,20 @@ while true
   d = rand(10);
   if ((a + b) >= 10) && ((c + d) >= 10)
     shiki = "#{a}#{c} + #{b}#{d}"
-    puts "#{idx}. #{shiki}"
     arr.push({ shiki: shiki, ans: Integer("#{a}#{c}") + Integer("#{b}#{d}") })
     idx += 1
     break if idx == 100
   end
 end
 
-arr.each_with_index {|o,i| puts "#{i+1}. #{o[:ans]}"}
+if (!params.key?(:questions) && !params.key?(:answers)) || params.key?(:questions)
+  puts "問題"
+  arr.each_with_index {|o,i| puts "#{i+1}. #{o[:shiki]}"}
+end
+
+puts
+
+if (!params.key?(:questions) && !params.key?(:answers)) || params.key?(:answers)
+  puts "答え"
+  arr.each_with_index {|o,i| puts "#{i+1}. #{o[:ans]}"}
+end
